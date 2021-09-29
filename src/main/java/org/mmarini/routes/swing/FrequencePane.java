95786c389369fdec12f721bec417331081bf2b24
mmarini <marco.marini@mmarini.org>
2021-09-29 19:11:01 +0200

    snapshot

--- src/main/java/org/mmarini/routes/swing/FrequencePane.java
+++ src/main/java/org/mmarini/routes/swing/FrequencePane.java
@@ -3,18 +3,12 @@
  */
 package org.mmarini.routes.swing;
 
+import hu.akarnokd.rxjava3.swing.SwingObservable;
+
+import javax.swing.*;
 import java.text.NumberFormat;
 import java.util.Dictionary;
 import java.util.Hashtable;
-
-import javax.swing.BoundedRangeModel;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.DefaultBoundedRangeModel;
-import javax.swing.JComponent;
-import javax.swing.JLabel;
-import javax.swing.JSlider;
-import javax.swing.SwingConstants;
 
 /**
  * Pannello di selezione del profilo di generazione delle mappe casuali
@@ -23,67 +17,66 @@ import javax.swing.SwingConstants;
  *
  */
 public class FrequencePane extends Box {
-	private static final double[] FREQUENCES = { 0.4, 0.7, 1.5, 2.2, 10. / 3. };
-	private static final long serialVersionUID = 1L;
+    private static final double[] FREQUENCES = {0.4, 0.7, 1.5, 2.2, 10. / 3.};
+    private static final long serialVersionUID = 1L;
+    private final BoundedRangeModel frequenceModel;
 
-	private final BoundedRangeModel frequenceModel;
+    /**
+     *
+     */
+    public FrequencePane() {
+        super(BoxLayout.PAGE_AXIS);
+        frequenceModel = new DefaultBoundedRangeModel(0, 0, 0, FREQUENCES.length - 1);
+        createContent();
+    }
 
-	/**
-	     *
-	     */
-	public FrequencePane() {
-		super(BoxLayout.PAGE_AXIS);
-		frequenceModel = new DefaultBoundedRangeModel(0, 0, 0, FREQUENCES.length - 1);
-		createContent();
-	}
+    /**
+     *
+     *
+     */
+    private void createContent() {
+        final Box box = createHorizontalBox();
+        box.add(new JLabel(Messages.getString("FrequencePane.frequenceLabel.text"))); //$NON-NLS-1$
+        box.add(createHorizontalGlue());
+        final JSlider freqSlider = new JSlider(SwingConstants.HORIZONTAL);
+        freqSlider.setModel(frequenceModel);
+        freqSlider.setMajorTickSpacing(1);
+        freqSlider.setPaintLabels(true);
+        freqSlider.setPaintTicks(true);
+        freqSlider.setPaintTrack(false);
+        freqSlider.setSnapToTicks(true);
+        final Dictionary<Integer, JComponent> labels = new Hashtable<Integer, JComponent>();
+        final NumberFormat format = NumberFormat.getNumberInstance();
+        for (int i = 0; i < FREQUENCES.length; ++i) {
+            labels.put(i, new JLabel(format.format(FREQUENCES[i])));
+        }
+        freqSlider.setLabelTable(labels);
+        box.add(freqSlider);
+        add(box);
+    }
 
-	/**
-	     *
-	     *
-	     */
-	private void createContent() {
-		final Box box = createHorizontalBox();
-		box.add(new JLabel(Messages.getString("FrequencePane.frequenceLabel.text"))); //$NON-NLS-1$
-		box.add(createHorizontalGlue());
-		final JSlider freqSlider = new JSlider(SwingConstants.HORIZONTAL);
-		freqSlider.setModel(frequenceModel);
-		freqSlider.setMajorTickSpacing(1);
-		freqSlider.setPaintLabels(true);
-		freqSlider.setPaintTicks(true);
-		freqSlider.setPaintTrack(false);
-		freqSlider.setSnapToTicks(true);
-		final Dictionary<Integer, JComponent> labels = new Hashtable<Integer, JComponent>();
-		final NumberFormat format = NumberFormat.getNumberInstance();
-		for (int i = 0; i < FREQUENCES.length; ++i) {
-			labels.put(i, new JLabel(format.format(FREQUENCES[i])));
-		}
-		freqSlider.setLabelTable(labels);
-		box.add(freqSlider);
-		add(box);
-	}
+    /**
+     *
+     * @return
+     */
+    public double getFrequence() {
+        return FREQUENCES[frequenceModel.getValue()];
+    }
 
-	/**
-	 *
-	 * @return
-	 */
-	public double getFrequence() {
-		return FREQUENCES[frequenceModel.getValue()];
-	}
-
-	/**
-	 *
-	 * @param frequence
-	 */
-	public void setFrequence(final double frequence) {
-		int idx = 0;
-		double error = Double.POSITIVE_INFINITY;
-		for (int i = 0; i < FREQUENCES.length; ++i) {
-			final double e = Math.abs(frequence - FREQUENCES[i]);
-			if (e < error) {
-				idx = i;
-				error = e;
-			}
-		}
-		frequenceModel.setValue(idx);
-	}
+    /**
+     *
+     * @param frequence
+     */
+    public void setFrequence(final double frequence) {
+        int idx = 0;
+        double error = Double.POSITIVE_INFINITY;
+        for (int i = 0; i < FREQUENCES.length; ++i) {
+            final double e = Math.abs(frequence - FREQUENCES[i]);
+            if (e < error) {
+                idx = i;
+                error = e;
+            }
+        }
+        frequenceModel.setValue(idx);
+    }
 }
--- src/main/java/org/mmarini/routes/swing/OptimizePane.java
+++ src/main/java/org/mmarini/routes/swing/OptimizePane.java
@@ -9,45 +9,35 @@
  */
 package org.mmarini.routes.swing;
 
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JCheckBox;
-import javax.swing.JLabel;
-import javax.swing.JSpinner;
-import javax.swing.SpinnerNumberModel;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
+import javax.swing.*;
+
+import static hu.akarnokd.rxjava3.swing.SwingObservable.change;
 
 /**
  * @author marco.marini@mmarini.org
  * @version $Id: OptimizePane.java,v 1.6 2010/10/19 20:32:59 marco Exp $
- *
  */
 public class OptimizePane extends Box {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
+    private final JCheckBox optimizeSpeed;
+    private final JCheckBox optimizeNodes;
+    private final JSpinner speedField;
+    private final JLabel speedLabel;
 
-	private final JCheckBox optimizeSpeed;
+    /**
+     *
+     */
+    public OptimizePane() {
+        super(BoxLayout.PAGE_AXIS);
+        optimizeSpeed = new JCheckBox(Messages.getString("OptimizePane.optimizeSpeed.label")); //$NON-NLS-1$
+        optimizeNodes = new JCheckBox(Messages.getString("OptimizePane.optimizeNodes.label")); //$NON-NLS-1$
+        speedLabel = new JLabel(Messages.getString("OptimizePane.speed.label")); //$NON-NLS-1$
+        speedField = new JSpinner();
 
-	private final JCheckBox optimizeNodes;
+        speedField.setModel(new SpinnerNumberModel(130., 10., 300., 10.));
 
-	private final JSpinner speedField;
-
-	private final JLabel speedLabel;
-
-	/**
-	     *
-	     */
-	public OptimizePane() {
-		super(BoxLayout.PAGE_AXIS);
-		optimizeSpeed = new JCheckBox(Messages.getString("OptimizePane.optimizeSpeed.label")); //$NON-NLS-1$
-		optimizeNodes = new JCheckBox(Messages.getString("OptimizePane.optimizeNodes.label")); //$NON-NLS-1$
-		speedLabel = new JLabel(Messages.getString("OptimizePane.speed.label")); //$NON-NLS-1$
-		speedField = new JSpinner();
-
-		speedField.setModel(new SpinnerNumberModel(130., 10., 300., 10.));
-
+		/*
 		optimizeSpeed.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(final ChangeEvent e) {
@@ -55,65 +45,65 @@ public class OptimizePane extends Box {
 			}
 
 		});
-		optimizeSpeed.setSelected(true);
-		optimizeNodes.setSelected(true);
-		speedField.setValue(130.);
-		createContext();
-	}
+		*/
+        optimizeSpeed.setSelected(true);
+        optimizeNodes.setSelected(true);
+        speedField.setValue(130.);
+        createContext();
+        change(optimizeSpeed)
+                .doOnNext(e -> handleOptimizeSpeedChange())
+                .subscribe();
+    }
 
-	/**
-	     *
-	     */
-	private void createContext() {
-		Box box = Box.createHorizontalBox();
-		box.add(optimizeNodes);
-		box.add(Box.createGlue());
-		add(box);
+    /**
+     *
+     */
+    private void createContext() {
+        Box box = Box.createHorizontalBox();
+        box.add(optimizeNodes);
+        box.add(Box.createGlue());
+        add(box);
 
-		box = Box.createHorizontalBox();
-		box.add(optimizeSpeed);
-		box.add(Box.createGlue());
-		add(box);
+        box = Box.createHorizontalBox();
+        box.add(optimizeSpeed);
+        box.add(Box.createGlue());
+        add(box);
 
-		box = Box.createHorizontalBox();
-		box.setBorder(BorderFactory.createTitledBorder(Messages.getString("OptimizePane.speedPane.title"))); //$NON-NLS-1$
-		box.add(speedLabel);
-		box.add(speedField);
-		box.add(Box.createGlue());
-		add(box);
-	}
+        box = Box.createHorizontalBox();
+        box.setBorder(BorderFactory.createTitledBorder(Messages.getString("OptimizePane.speedPane.title"))); //$NON-NLS-1$
+        box.add(speedLabel);
+        box.add(speedField);
+        box.add(Box.createGlue());
+        add(box);
+    }
 
-	/**
-	 *
-	 * @return
-	 */
-	public double getSpeedLimit() {
-		return ((Number) speedField.getValue()).doubleValue() / 3.6f;
-	}
+    /**
+     * @return
+     */
+    public double getSpeedLimit() {
+        return ((Number) speedField.getValue()).doubleValue() / 3.6f;
+    }
 
-	/**
-	     *
-	     *
-	     */
-	private void handleOptimizeSpeedChange() {
-		final boolean selected = isOptimizeSpeed();
-		speedLabel.setEnabled(selected);
-		speedField.setEnabled(selected);
-	}
+    /**
+     *
+     */
+    private void handleOptimizeSpeedChange() {
+        final boolean selected = isOptimizeSpeed();
+        speedLabel.setEnabled(selected);
+        speedField.setEnabled(selected);
+    }
 
-	/**
-	 *
-	 * @return
-	 */
-	public boolean isOptimizeNodes() {
-		return optimizeNodes.isSelected();
-	}
+    /**
+     * @return
+     */
+    public boolean isOptimizeNodes() {
+        return optimizeNodes.isSelected();
+    }
 
-	/**
-	 *
-	 * @return
-	 */
-	public boolean isOptimizeSpeed() {
-		return optimizeSpeed.isSelected();
-	}
+    /**
+     * @return
+     */
+    public boolean isOptimizeSpeed() {
+        return optimizeSpeed.isSelected();
+    }
 }
